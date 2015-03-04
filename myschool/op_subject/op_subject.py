a51e3ad074b0d0e8d1a17adfc772d2e6821bc8df
hansanie <hansanie@openarc.lk>
2015-03-04 15:46:52 +0530

    Resized field size Subject Added Groupby search view for xml  modify the menu title

--- myschool/op_subject/op_subject.py
+++ myschool/op_subject/op_subject.py
@@ -3,10 +3,9 @@ from openerp.osv import osv, fields
 class op_subject(osv.Model):
     _name = 'op.subject'
     _columns = {
-        'name': fields.char(size=25, string='Name', required=True),
-        'code': fields.char(size=15, string='Code', required=True),
-        'type': fields.selection([('t', 'Theory'), ('p', 'Practical'),  ('pt','Both'), ('o', 'Other')], string='Type',
-                                 required=True),
+        'name': fields.char(size=50, string='Name', required=True),
+        'code': fields.char(size=25, string='Code', required=True),
+        'type': fields.selection([('t', 'Theory'), ('p', 'Practical'),  ('pt','Both'), ('o', 'Other')], string='Type'),
         'standard_id': fields.many2one('op.standard', 'Standard', required=True),
 
     }
