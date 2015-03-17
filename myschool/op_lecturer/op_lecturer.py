67d0b2658ea46d86baa262268b5d6167910b8cc9
hansanie <hansanie@openarc.lk>
2015-03-17 10:25:48 +0530

    added kanban view for student & lecturer modify lecturer fields modify menus

--- myschool/op_lecturer/op_lecturer.py
+++ myschool/op_lecturer/op_lecturer.py
@@ -7,13 +7,12 @@ class op_lecturer(osv.Model):
     _columns = {
         'partner_id': fields.many2one('res.partner', 'Partner', required=True, ondelete="restrict"),
         'birth_date': fields.date(string='Birth Date', required=True),
-        'category': fields.selection([('fulltime', 'Full Time'), ('visiting', 'Visiting'), ('parttime', 'Part Time')], string='Category'),
+        'category': fields.selection([('parttime', 'Part Time'), ('visiting', 'Visiting'), ('fulltime', 'Full Time')], string='Category', required=True),
         'gender': fields.selection([('m','Male'),('f','Female')], string='Gender', required=True),
         'language': fields.selection([('sinhala', 'Sinhala'), ('english', 'English'), ('tamil', 'Tamil')], string='Language'),
         'bank_acc_num': fields.char(size=64, string='Bank Acc Number'),
         'lecturer_subject_ids': fields.many2many('op.subject', 'lecturer_subject_rel', 'op_lecturer_id', 'op_subject_id', string='Subjects'),
-        # 'partner': fields.related('partner_id', 'name', string='Related Supplier', type='char', readonly=True),
-
+        # 'mobile_no': fields.char(size=15, string='Mobile Number', required=True),
 
     }
 
