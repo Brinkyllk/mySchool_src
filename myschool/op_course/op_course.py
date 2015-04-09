da5b1df85e34795389e170482309db19d3c90a53
Sakvith Jayasinghe <sakvith@openarc.lk>
2015-04-09 11:31:22 +0530

    changed course product creation methodology

--- myschool/op_course/op_course.py
+++ myschool/op_course/op_course.py
@@ -1,22 +1,21 @@
 from openerp.osv import osv, fields
+from openerp import api
 # from tools.translate import _
 
 
 class op_course(osv.Model):
     _name = 'op.course'
     # to do (Related Field)
-    _inherits = {'product.product': 'product_id'}
 
     _columns = {
         'code': fields.char(size=8, string='Code', required=True),
+        'name': fields.char(string='Name', required=True),
         'level': fields.selection([('certification', 'Certification'), ('diploma', 'Diploma'), ('degree', 'Degree')],
                                   string='Course Level'),
-        # 'subject_ids': fields.many2many('op.subject', 'op_course_subject_rel', 'course_id',
-        #                                 'subject_id', string='Subject(s)'),
-        'product_id': fields.many2one('product.product', 'Product', select=True, ondelete='restrict', readonly=True),
-        'product': fields.related('product_id', 'name', string='Course', type='char', readonly=True),
+        'product_id': fields.many2one('product.product', 'Product', ondelete='restrict'),
+        'price': fields.related('product_id', 'list_price', string='Price', type='char'),
         'subject_ids': fields.many2many('op.subject', 'op_course_subject_rel', 'course_id', 'subject_id',
-                                        string='Subjects'),
+                                        string='Subject(s)'),
 
 
         # ----------- test ----------
@@ -24,3 +23,17 @@ class op_course(osv.Model):
     }
 
     _sql_constraints = [('code', 'UNIQUE (code)', 'The CODE of the COURSE must be unique!')]
+
+
+    def create(self, cr, uid, vals, context = None):
+        #Reffer producy
+        productRef = self.pool.get('product.product')
+
+        product = {'name': vals['name'], 'list_price': vals['price']}
+        pid = productRef.create(cr, uid, product, context=context)
+        vals.update({'product_id': pid})
+
+        return super(op_course, self).create(cr, uid, vals, context=context)
+
+
+
