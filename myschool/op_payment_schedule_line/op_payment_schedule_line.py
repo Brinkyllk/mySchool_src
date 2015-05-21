58635fe2996d28c6a0e9f3bfac7a5c398489644b
randika <randika@openarc.lk>
2015-05-21 10:50:15 +0530

    Modify the read method of the payment schedule Add two new fields to the Payment Schedule line Modify the view

--- myschool/op_payment_schedule_line/op_payment_schedule_line.py
+++ myschool/op_payment_schedule_line/op_payment_schedule_line.py
@@ -5,8 +5,9 @@ class op_payment_schedule_line(osv.Model):
     _name = 'op.payment.schedule.line'
     _columns = {
                 'schedule_id': fields.many2one('op.payment.schedule', 'Payment Schedule'),
+                'full_name': fields.char('Name'),
+                'stu_reg_no': fields.char('Reg_No'),
                 'due_date': fields.date('Due Date'),
-                'amount': fields.float('Amount'),
+                'amount': fields.float('Amount')
                 }
-
 
