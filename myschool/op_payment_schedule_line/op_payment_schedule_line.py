3a4875a116d6d82c0c90d2e2af3f5f0db4a3467b
hansanie <hansanie@openarc.lk>
2015-05-21 11:09:51 +0530

    Modify field name Added groupby and search

--- myschool/op_payment_schedule_line/op_payment_schedule_line.py
+++ myschool/op_payment_schedule_line/op_payment_schedule_line.py
@@ -5,8 +5,8 @@ class op_payment_schedule_line(osv.Model):
     _name = 'op.payment.schedule.line'
     _columns = {
                 'schedule_id': fields.many2one('op.payment.schedule', 'Payment Schedule'),
-                'full_name': fields.char('Name'),
-                'stu_reg_no': fields.char('Reg_No'),
+                'full_name': fields.char('Student Name'),
+                'stu_reg_no': fields.char('Student No'),
                 'due_date': fields.date('Due Date'),
                 'amount': fields.float('Amount')
                 }
