3187da0c904a6668e0360488a9ae5ab8ee15917a
hansanie <hansanie@openarc.lk>
2015-05-27 12:38:44 +0530

    Added create_invoice function for student

--- myschool/op_student/op_student.py
+++ myschool/op_student/op_student.py
@@ -3,7 +3,7 @@ from datetime import date, datetime
 from openerp.tools.translate import _
 from validate_email import validate_email
 import re
-
+import time
 
 class op_student(osv.Model):
     # def _get_def_batch(self, cr, uid, ids, field_name, arg, context=None):
@@ -276,6 +276,51 @@ class op_student(osv.Model):
             'nodestroy': True,
             'domain': domain,
                 }
+
+
+    def create_invoice(self, cr, uid, ids, context={}):
+        """ Create invoice for fee payment process of student """
+
+        invoice_pool = self.pool.get('account.invoice')
+
+        default_fields = invoice_pool.fields_get(cr, uid, context=context)
+        invoice_default = invoice_pool.default_get(cr, uid, default_fields, context=context)
+
+        for student in self.browse(cr, uid, ids, context=context):
+
+            onchange_partner = invoice_pool.onchange_partner_id(cr, uid, [], type='out_invoice', \
+                                partner_id=student.partner_id.id)
+            invoice_default.update(onchange_partner['value'])
+
+
+            invoice_data = {
+                            'partner_id': student.partner_id.id,
+                            'date_invoice': time.strftime('%Y-%m-%d'),
+                            # 'payment_term': student.standard_id.payment_term and student.standard_id.payment_term.id or student.course_id.payment_term and student.course_id.payment_term.id or False,
+                            }
+
+        invoice_default.update(invoice_data)
+        invoice_id = invoice_pool.create(cr, uid, invoice_default, context=context)
+
+        models_data = self.pool.get('ir.model.data')
+        form_view = models_data.get_object_reference(cr, uid, 'account', 'invoice_form')
+        tree_view = models_data.get_object_reference(cr, uid, 'account', 'invoice_tree')
+        value = {
+                'domain': str([('id', '=', invoice_id)]),
+                'view_type': 'form',
+                'view_mode': 'form',
+                'res_model': 'account.invoice',
+                'view_id': False,
+                'views': [(form_view and form_view[1] or False, 'form'),
+                          (tree_view and tree_view[1] or False, 'tree')],
+                'type': 'ir.actions.act_window',
+                'res_id': invoice_id,
+                'target': 'current',
+                'nodestroy': True
+            }
+        return value
+
+
 
     _constraints = [
         (_check_birthday, 'Birth Day cannot be future date!', ['birth_date']),
