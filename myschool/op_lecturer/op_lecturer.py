d3a205e1ca54f5d2c72a9ac3c25af00b2cb2940e
rukshani <rukshani@openarc.lk>
2015-05-27 15:49:03 +0530

    Add bank name

--- myschool/op_lecturer/op_lecturer.py
+++ myschool/op_lecturer/op_lecturer.py
@@ -44,6 +44,8 @@ class op_lecturer(osv.Model):
         'gender': fields.selection([('m', 'Male'), ('f', 'Female')], string='Gender', required=True),
         'language': fields.selection([('sinhala', 'Sinhala'), ('english', 'English'), ('tamil', 'Tamil')],
                                      string='Language'),
+        'bank': fields.selection([('BOC', 'Bank of Ceylon'), ('HSBC', 'HSBC'), ('Commercial Bank', 'Commercial Bank'),
+                                  ('Sampath Bank', 'Sampath Bank'), ('Peoples Bank', 'Peoples Bank')], string='Bank'),
         'bank_acc_num': fields.char(size=64, string='Bank Acc Number'),
         'lecturer_subject_ids': fields.many2many('op.subject', 'lecturer_subject_rel', 'op_lecturer_id',
                                                  'op_subject_id', string='Subjects'),
@@ -122,5 +124,16 @@ class op_lecturer(osv.Model):
         (_check_birthday, 'Birth Day cannot be future date!', ['birth_date']),
     ]
 
+# class op_lecturer_bank(osv.osv):
+#     _name = 'op.lecturer.bank'
+#     _order = 'name'
+#     _columns = {
+#         'name': fields.char('Bank', required=True, translate=True),
+#         'shortcut': fields.char('Account Number', translate=True),
+        # 'domain': fields.selection([('partner', 'Partner'), ('contact', 'Contact')], 'Domain', required=True)
+    # }
+    # _defaults = {
+    #     'domain': 'contact',
+    # }
 
 
