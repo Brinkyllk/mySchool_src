c9c2dea05e9ee67e5373d0f5c72a4a88d1096082
lukshala <lukshala@openarc.lk>
2015-05-28 10:17:28 +0530

    rel_subjects- removed 'required=true'

--- myschool/op_lecturer/op_lecturer.py
+++ myschool/op_lecturer/op_lecturer.py
@@ -47,6 +47,13 @@ class op_lecturer(osv.Model):
         'bank': fields.selection([('BOC', 'Bank of Ceylon'), ('HSBC', 'HSBC'), ('Commercial Bank', 'Commercial Bank'),
                                   ('Sampath Bank', 'Sampath Bank'), ('Peoples Bank', 'Peoples Bank')], string='Bank'),
         'bank_acc_num': fields.char(size=64, string='Bank Acc Number'),
+
+        'address_line1': fields.char('address line1', size=20),
+        'address_line2': fields.char('address line2', size=25),
+        'town': fields.char('town', size=25),
+        'province': fields.char('province', size=20),
+        'nation': fields.char('nation', size=20),
+
         'lecturer_subject_ids': fields.many2many('op.subject', 'lecturer_subject_rel', 'op_lecturer_id',
                                                  'op_subject_id', string='Subjects'),
         'phone': fields.char(string='Phone Number', size=256),
@@ -67,6 +74,52 @@ class op_lecturer(osv.Model):
             raise osv.except_osv('Invalid NIC', 'Please enter a valid NIC')
         return True
 
+    #------check spaces in address line one----#
+    def _check_add_l_one(self, cr, uid, ids, context=None):
+        obj = self.browse(cr, uid, ids, context=context)
+        value = str(obj.address_line1)
+        if not value:
+            return False
+        else:
+            return True
+
+    #------check spaces in address line two----#
+    def _check_add_l_two(self, cr, uid, ids, context=None):
+        obj = self.browse(cr, uid, ids, context=context)
+        value = str(obj.address_line2)
+        if not value:
+            return False
+        else:
+            return True
+
+    #-----check spaces in town-----------------#
+    def _check_town(self, cr, uid, ids, context=None):
+        obj = self.browse(cr, uid, ids, context=context)
+        value = str(obj.town)
+        if not value:
+            return False
+        else:
+            return True
+
+    #-----check spaces in province--------------#
+    def _check_province(self, cr, uid, ids, context=None):
+        obj = self.browse(cr, uid, ids, context=context)
+        value = str(obj.province)
+        if not value:
+            return False
+        else:
+            return True
+
+    #-----check spaces in country---------------#
+    def _check_nation(self, cr, uid, ids, context=None):
+        obj = self.browse(cr, uid, ids, context=context)
+        value = str(obj.nation)
+        new_value = value.replace(" ", "")
+        if not value or not new_value.isalpha():
+            return False
+        else:
+            return True
+
     # overriding create method
     def create(self, cr, uid, vals, context=None):
         vals.update({'supplier': True, 'customer': False})
@@ -84,6 +137,27 @@ class op_lecturer(osv.Model):
         # NIC validation on create
         if 'id_number' in vals:
             self.validate_NIC(cr, uid, [], vals['id_number'])
+
+        if 'address_line1' in vals:
+            line1 = vals['address_line1'].strip()
+            vals.update({'address_line1': line1, 'street':line1})
+            # vals.update({'street': vals['address_line1']})
+
+        if 'address_line2' in vals:
+            line2 = vals['address_line2'].strip()
+            vals.update({'street2': line2, 'address_line2': line2})
+
+        if 'town' in vals:
+            twn = vals['town'].strip()
+            vals.update({'city': twn, 'town': twn})
+
+        if 'province' in vals:
+            prvn = vals['province'].strip()
+            vals.update({'province': prvn})
+
+        if 'nation' in vals:
+            cntry = vals['nation'].strip()
+            vals.update({'nation': cntry})
 
         res = super(op_lecturer, self).create(cr, uid, vals, context=context)
         return res
@@ -105,6 +179,26 @@ class op_lecturer(osv.Model):
         if 'id_number' in values:
             self.validate_NIC(cr, uid, [], values['id_number'])
 
+        if 'address_line1' in values:
+            line1 = values['address_line1'].strip()
+            values.update({'street': line1, 'address_line1': line1})
+
+        if 'address_line2' in values:
+            line2 = values['address_line2'].strip()
+            values.update({'street2': line2, 'address_line2': line2})
+
+        if 'town' in values:
+            twn = values['town'].strip()
+            values.update({'city': twn, 'town': twn})
+
+        if 'province' in values:
+            prvn = values['province'].strip()
+            values.update({'province': prvn})
+
+        if 'nation' in values:
+            cntry = values['nation'].strip()
+            values.update({'nation': cntry})
+
         res = super(op_lecturer, self).write(cr, uid, ids, values, context=context)
         return res
 
@@ -122,6 +216,11 @@ class op_lecturer(osv.Model):
 
     _constraints = [
         (_check_birthday, 'Birth Day cannot be future date!', ['birth_date']),
+        (_check_add_l_one, 'Entered Invalid Data in Address !!', ['address_line1']),
+        (_check_add_l_two, 'Entered Invalid Data in Address !!', ['address_line2']),
+        (_check_town, 'Entered Invalid Data in Address !!', ['town']),
+        (_check_province, 'Entered Invalid Data in Address !!', ['province']),
+        (_check_nation, 'Entered Invalid Data in Address !!', ['nation']),
     ]
 
 # class op_lecturer_bank(osv.osv):
