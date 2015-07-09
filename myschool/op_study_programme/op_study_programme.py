192b43672246dca20f3f2020a5ae273289d90414
rukshani <rukshani@openarc.lk>
2015-07-09 15:45:17 +0530

    Study Programme

--- myschool/op_study_programme/op_study_programme.py
+++ myschool/op_study_programme/op_study_programme.py
@@ -5,8 +5,8 @@ from openerp import api
 class op_study_programme(osv.Model):
     _name = 'op.study.programme'
     _columns = {
-        'code': fields.char('Code', required=True, size=8),
+        'code': fields.char('Code', required=True, size=20),
         'name': fields.char('Name',required=True, size=100),
-        'res_person': fields.char('Responsible Person', required=True),
-        'parent': fields.char('Parent', required=True),
+        'res_person': fields.fields.many2one('res.partner', 'Responsible Persona', required=True, ondelete="restrict"),
+        'parent': fields.many2one('op.study.programme', 'Parent'),
     }
