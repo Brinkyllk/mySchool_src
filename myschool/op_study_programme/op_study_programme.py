9252a86fd39e750c96cb086b023f2a7e53f617b7
randika <randika@openarc.lk>
2015-07-09 16:01:16 +0530

    op_study_programme modification

--- myschool/op_study_programme/op_study_programme.py
+++ myschool/op_study_programme/op_study_programme.py
@@ -7,6 +7,6 @@ class op_study_programme(osv.Model):
     _columns = {
         'code': fields.char('Code', required=True, size=20),
         'name': fields.char('Name',required=True, size=100),
-        'res_person': fields.many2one('res.partner', 'Responsible Persona', required=True, ondelete="restrict"),
+        'res_person': fields.many2one('res.partner', 'Responsible Person', required=True, ondelete="restrict"),
         'parent': fields.many2one('op.study.programme', 'Parent'),
     }
