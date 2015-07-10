6477760d2df4f2cc4c10eb5d115ce82c744bf1aa
rukshani <rukshani@openarc.lk>
2015-07-10 16:57:32 +0530

    Upper Case the CODE

--- myschool/op_study_programme/op_study_programme.py
+++ myschool/op_study_programme/op_study_programme.py
@@ -3,10 +3,23 @@ from openerp.osv import osv
 from openerp import api
 
 class op_study_programme(osv.Model):
+
+    #--Code change to upper case---
+    @api.onchange('code')
+    def onchange_case(self, cr, uid, ids, code):
+        if code != False:
+            result = {'value': {
+                'code': str(code).upper()
+            }
+            }
+            return result
+        else:
+            return True
+
     _name = 'op.study.programme'
     _columns = {
         'code': fields.char('Code', required=True, size=20),
         'name': fields.char('Name',required=True, size=100),
-        'res_person': fields.many2one('res.partner', 'Responsible Person', required=True, ondelete="restrict"),
+        'res_person': fields.many2one('res.partner', 'Responsible Person'),
         'parent': fields.many2one('op.study.programme', 'Parent'),
     }
