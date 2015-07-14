2af711e83f2750637245b8a8a26daf486db21c81
randikaa <randika@openarc.lk>
2015-07-14 11:57:45 +0530

    loaded student profile from registration

--- myschool/op_registration/op_registration.py
+++ myschool/op_registration/op_registration.py
@@ -37,10 +37,10 @@ class op_registration(osv.osv):
         return super(op_registration, self).copy(cr, uid, id, default, context=context)
 
     _columns = {
-        'name': fields.char(size=128, string='First Name', required=True, states={'done': [('readonly', True)]}),
-        'middle_name': fields.char(size=128, string='Middle Name',
+        'first_name': fields.char(size=15, string='First Name', required=True, states={'done': [('readonly', True)]}),
+        'middle_name': fields.char(size=15, string='Middle Name',
                                    states={'done': [('readonly', True)]}),
-        'last_name': fields.char(size=128, string='Last Name', required=True, states={'done': [('readonly', True)]}),
+        'last_name': fields.char(size=20, string='Last Name', required=True, states={'done': [('readonly', True)]}),
         'title': fields.many2one('res.partner.title', 'Title', states={'done': [('readonly', True)]}),
         'application_number': fields.char(size=16, string='Application Number', required=True,
                                           states={'done': [('readonly', True)]}),
@@ -48,42 +48,26 @@ class op_registration(osv.osv):
         'application_date': fields.datetime(string='Application Date', required=True,
                                             states={'done': [('readonly', True)]}),
         'birth_date': fields.date(string='Birth Date', required=True, states={'done': [('readonly', True)]}),
-        # 'course_id': fields.many2one('op.course', string='Course', required=True,
-        #                              states={'done': [('readonly', True)]}),
-        # 'batch_id': fields.many2one('op.batch', string='Batch', required=True, states={'done': [('readonly', True)]}),
-        'street': fields.char(size=256, string='Street', states={'done': [('readonly', True)]}),
-        'street2': fields.char(size=256, string='Street2', states={'done': [('readonly', True)]}),
-        'phone': fields.char(size=16, string='Phone', states={'done': [('readonly', True)]}),
-        'mobile': fields.char(size=16, string='Mobile', states={'done': [('readonly', True)]}),
-        'email': fields.char(size=256, string='Email', states={'done': [('readonly', True)]}),
-        'city': fields.char(size=64, string='City', states={'done': [('readonly', True)]}),
-        'zip': fields.char(size=8, string='Zip', states={'done': [('readonly', True)]}),
-        # 'state_id': fields.many2one('res.country.state', string='States', states={'done': [('readonly', True)]}),
-        # 'country_id': fields.many2one('res.country', string='Country', states={'done': [('readonly', True)]}),
+
+        'address_line1': fields.char('address line1', size=20, states={'done': [('readonly', True)]}),
+        'address_line2': fields.char('address line2', size=25, states={'done': [('readonly', True)]}),
+        'town': fields.char('town', size=25, states={'done': [('readonly', True)]}),
+        'province': fields.char('province', size=20, states={'done': [('readonly', True)]}),
+        'nation': fields.char('nation', size=20, states={'done': [('readonly', True)]}),
+
         'fees': fields.float(string='Fees', states={'done': [('readonly', True)]}),
         'photo': fields.binary(string='Photo', states={'done': [('readonly', True)]}),
         'state': fields.selection(
             [('d', 'Draft'), ('i', 'Confirm'), ('s', 'Enroll'), ('done', 'Done'), ('r', 'Rejected'), ('p', 'Pending'),
              ('c', 'Cancel')], readonly=True, select=True, string='State'),
         'due_date': fields.date(string='Due Date', states={'done': [('readonly', True)]}),
-        # 'prev_institute': fields.char(size=256, string='Previous Institute', states={'done': [('readonly', True)]}),
-        # 'prev_course': fields.char(size=256, string='Previous Course', states={'done': [('readonly', True)]}),
-        # 'prev_result': fields.char(size=256, string='Previous Result', states={'done': [('readonly', True)]}),
         'family_business': fields.char(size=256, string='Family Business', states={'done': [('readonly', True)]}),
         'family_income': fields.float(string='Family Income', states={'done': [('readonly', True)]}),
-        # 'religion_id': fields.many2one('op.religion', string='Religion', states={'done': [('readonly', True)]}),
-        # 'category_id': fields.many2one('op.category', string='Category', required=True, states={'done': [('readonly', True)]}),
         'gender': fields.selection([('m', 'Male'), ('f', 'Female'), ('o', 'Other')], string='Gender', required=True,
                                    states={'done': [('readonly', True)]}),
-        # 'standard_id': fields.many2one('op.standard', string='Standard', required=True,
-        #                                states={'done': [('readonly', True)]}),
         'division_id': fields.many2one('op.division', string='Division', states={'done': [('readonly', True)]}),
         'student_id': fields.many2one('op.student', string='Student', states={'done': [('readonly', True)]}),
         'nbr': fields.integer('# of Registration', readonly=True),
-        'gr_no': fields.boolean('Old Student??'),
-        'gr_no_old': fields.char(string="GR Number old", size=10),
-        'gr_no_new': fields.char(string="GR Number new", size=10),
-
     }
 
     _defaults = {
@@ -95,7 +79,6 @@ class op_registration(osv.osv):
 
     _order = "application_number desc"
 
-
     def confirm_in_progress(self, cr, uid, ids, context=None):
         self.write(cr, uid, ids, {'state': 'i'})
         return True
@@ -105,29 +88,21 @@ class op_registration(osv.osv):
             context = {}
         student_pool = self.pool.get('op.student')
         for field in self.browse(cr, uid, ids, context=context):
-            if field.gr_no == True:
-                gr = field.gr_no_old
-            else:
-                gr = field.gr_no_new
             vals = {
                 'title': field.title and field.title.id or False,
-                'first_name': field.name,
+                'first_name': field.first_name,
                 'middle_name': field.middle_name,
                 'last_name': field.last_name,
                 'birth_date': field.birth_date,
                 'gender': field.gender,
                 'photo': field.photo or False,
-                'stu_reg_number': gr,
-                'address': [(0, 0, {
-                    'name': field.name or False,
-                    'type': 'invoice',
-                    'title': field.title and field.title.id or False,
-                    'address_line1': field.street or False,
-                    'address_line2': field.street2 or False,
-                    'phone': field.phone or False,
-                    'town': field.city or False,
-                })]
+                'address_line1': field.address_line1 or False,
+                'address_line2': field.address_line2 or False,
+                'town': field.town or False,
+                'province': field.province or False,
+                'nation': field.nation or False,
             }
+
         new_student = student_pool.create(cr, uid, vals, context=context)
         self.write(cr, uid, ids, {'state': 's', 'student_id': new_student, 'nbr': 1})
         return True
@@ -153,12 +128,11 @@ class op_registration(osv.osv):
         return True
 
     def open_student(self, cr, uid, ids, context={}):
-
         this_obj = self.browse(cr, uid, ids[0], context)
         student = self.pool.get('op.student').browse(cr, uid, this_obj.student_id.id, context)
         models_data = self.pool.get('ir.model.data')
-        form_view = models_data.get_object_reference(cr, uid, 'myschool', 'act_open_op_student_view_form')
-        tree_view = models_data.get_object_reference(cr, uid, 'myschool', 'act_open_op_student_view_tree')
+        form_view = models_data.get_object_reference(cr, uid, 'myschool', 'view_student_form')
+        tree_view = models_data.get_object_reference(cr, uid, 'myschool', 'view_student_tree')
         value = {
             'domain': str([('id', '=', student.id)]),
             'view_type': 'form',
@@ -169,8 +143,8 @@ class op_registration(osv.osv):
                       (tree_view and tree_view[1] or False, 'tree')],
             'type': 'ir.actions.act_window',
             'res_id': student.id,
-            'target': 'current',
-            'nodestroy': True
+            'target': 'new',
+            'nodestroy': True,
         }
         self.write(cr, uid, ids, {'state': 'done'})
         return value
