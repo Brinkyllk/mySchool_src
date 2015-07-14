107829e66985fee7366e822e7e4876ee1df1e941
rukshani <rukshani@openarc.lk>
2015-07-14 16:19:20 +0530

    Remove registration number

--- myschool/op_enrollment/op_enrollment.py
+++ myschool/op_enrollment/op_enrollment.py
@@ -6,7 +6,6 @@ class op_enrollment(osv.Model):
     _columns = {
         'student_id': fields.many2one('op.student', string="Student Name"),
         'batch_code': fields.many2one('op.batch', string="Batch"),
-        'registration_no': fields.many2one('op.registration', string="Registration No"),
         'lead_id': fields.many2one('crm.lead', string="Lead No"),
         'payment_term': fields.many2one('account.payment.term', 'Payment Terms'),
         'template_price': fields.many2one('op.batch', 'Template Price'),
